af17fd94b3b609ae01e1b2dac0ca844b50302ba8
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-08-14 21:04:14 +0300

    Initial commit

---
+++ rank.py
@@ -0,0 +1,12 @@
+def  exponentiation(inp_num,extent):# первое задание
+    if extent == 0:
+        return 1
+    if extent == 1:
+        return inp_num
+    return inp_num * exponentiation(inp_num,extent-1)
+
+def sum_values_of_num(inp_num):# второе задание
+    if inp_num == 0:
+        return inp_num
+    return inp_num % 10 + sum_values_of_num(inp_num // 10)
+print(sum_values_of_num(123456789))
